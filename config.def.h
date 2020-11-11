dbc4a28913fb1c76343d7ce1ec885e1f44a18291
mohad12211 <mohammedkaabi64@gmail.com>
2020-11-11 20:09:51 +0300

    Add spotify rule

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
