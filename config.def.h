cb648f2e989df07143dbafbca719ac1dfb44f808
mohad12211 <mohammedkaabi64@gmail.com>
2020-11-21 22:22:10 +0300

    change bar alpha

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const unsigned int baralpha = 222;
+static const unsigned int baralpha = 180;
 static const unsigned int borderalpha = OPAQUE;
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
